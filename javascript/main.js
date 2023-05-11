9b8ec7b9abec1bd50d1164a1e2e6631b5cb8b546
nanaapriani92 <129491512+nanaapriani92@users.noreply.github.com>
2023-05-11 15:47:54 +0700

    Add files via upload

---
+++ javascript/main.js
@@ -0,0 +1,12 @@
+function msglogin(){
+    alert("Successfully LOGIN");
+}
+function msgsignup(){
+    alert("Successfully SIGNUP");
+}
+function msgorder(){
+    alert("Thankyou For Your ORDER");
+}
+function msgjoin(){
+    alert("Thankyou For Your JOIN");
+}
